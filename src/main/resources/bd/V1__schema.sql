33ab8b76eea19a43e025227024af9d1b45e33c6a
Oscar Serrano <serranojaimesoscarsteven@hotmail.com>
2026-01-19 09:42:43 -0500

    Se creó y se verificó la funcionalidad de la tabla usuarios

--- src/main/resources/bd/V1__schema.sql
+++ src/main/resources/bd/V1__schema.sql
@@ -1,9 +1,19 @@
 SET search_path TO tienda;
 
+DROP TABLE IF EXISTS usuarios;
 DROP TABLE IF EXISTS rol;
 
 CREATE TABLE rol (
     id_rol SERIAL PRIMARY KEY,
     nombre_rol VARCHAR(50) NOT NULL UNIQUE,
     descripcion_rol TEXT
+);
+
+CREATE TABLE usuarios (
+    usuario_id SERIAL PRIMARY KEY,
+    nombre_usuario VARCHAR(100) NOT NULL UNIQUE,
+    email_usuario VARCHAR(150) NOT NULL UNIQUE,
+    password_usuario VARCHAR(255) NOT NULL,
+    activo BOOLEAN DEFAULT TRUE,
+    fecha_creacion TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
