22fb28b3badfd0a0df7b08a6e8e8448296ca4668
Oscar Serrano <serranojaimesoscarsteven@hotmail.com>
2026-01-20 10:54:53 -0500

    V2: roles base y usuario administrador

--- src/main/resources/bd/V2__data_inicial.sql
+++ src/main/resources/bd/V2__data_inicial.sql
@@ -6,8 +6,8 @@ INSERT INTO rol (nombre_rol, descripcion_rol)
 VALUES
     ('ADMIN', 'Administrador del sistema con todos los privilegios'),
     ('VENDEDOR', 'Usuario encargado de las ventas y atencion al cliente');
-SELECT * FROM rol;
+
+-- CREACION DE USUARIO ADMINISTRADOR
 INSERT INTO usuario (nombre_usuario, email_usuario, password_usuario, activo)
 VALUES
     ('admin', 'admin@tienda.com', 'PENDIENTE_IMPLEMENTAR_HASH', true);
-SELECT nombre_usuario, email_usuario, activo FROM usuario;
