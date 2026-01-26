d2b6ebd40952ee2b387a3e7ad269dfa805ffda37
Oscar Serrano <serranojaimesoscarsteven@hotmail.com>
2026-01-26 10:11:54 -0500

    feat: ProveedorService

--- src/main/java/com/tiendabarrio/service/ProveedorService.java
+++ src/main/java/com/tiendabarrio/service/ProveedorService.java
@@ -1,4 +1,13 @@
 package com.tiendabarrio.service;
 
-public class ProveedorService {
+import com.tiendabarrio.model.Proveedor;
+import java.util.List;
+
+public interface ProveedorService {
+    void crearProveedor(Proveedor proveedor);
+    Proveedor buscarProveedorPorId(int id);
+    List<Proveedor> listarTodosProveedores();
+    void actualizarProveedor(Proveedor proveedor);
+    void eliminarProveedor(int id);
+    Proveedor buscarProveedorPorEmail(String email);
 }
